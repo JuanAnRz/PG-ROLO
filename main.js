2f3d7f3ffab65d973419a39220eaa035c3186174
JuanAnRz <juancitoarodriguez1@gmail.com>
2024-12-01 14:28:26 -0300

    cel

---
+++ main.js
@@ -0,0 +1,7 @@
+let menu=document.getElementById('menu')
+// let menus=document.getElementById('menu2')
+let links=document.getElementById('links')
+
+menu.addEventListener('click',()=>{
+    links.classList.toggle('links')
+})
